ed81e495cfd3c35c271909931b434851fa90a41b
PRIYANKA <PRIYANKA@192.168.0.4>
2018-02-21 06:59:02 +0530

    First Commit

---
+++ webdriverTest/src/test/java/webdriverTest/NewTest.java
@@ -0,0 +1,29 @@
+package webdriverTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	
+	  WebDriver driver;		
+		@Test				
+		public void testEasy() {	
+			driver.get("http://demo.guru99.com/test/guru99home/");  
+			String title = driver.getTitle();				 
+			Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
+		}	
+		@BeforeTest
+		public void beforeTest() {
+			System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+		    driver = new ChromeDriver();  
+		}		
+		@AfterTest
+		public void afterTest() {
+			driver.quit();			
+		}		
+
+}
